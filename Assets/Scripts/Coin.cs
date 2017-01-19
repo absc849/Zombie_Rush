d335fc96aeb549fef0ce8e5efb2dfd19546fffe0
Brandon <brand.ld@1994@gmail.com>
2017-01-19 20:01:35 +0000

    Coins Move
    Created a new script and inherited moving object. 4 Coins move up and
    down whilst translating forward next is implementing the collision

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,39 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Rock : MovingObject {
+public class Coin : MovingObject {
 
 	[SerializeField]
 	Vector3 topPos;
-
+	
 	[SerializeField]
 	Vector3 bottPos;
-
+	
 	[SerializeField]
 	float speed;
-
-//	[SerializeField]
-//	float rotateSpeed;
-
-
-
+	
+	//	[SerializeField]
+	//	float rotateSpeed;
+	
+	
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 		StartCoroutine (Move (bottPos));
 	}
+	
 
 	protected override void Update()
 	{
-
+		
 		if (GameManager.instance.PlayerActive) {
 			//makes rocks fly off screen :(
 			//transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
-
+			
 			base.Update ();
 		}
 	}
-
 
 
 	IEnumerator Move(Vector3 target){
@@ -41,30 +41,30 @@ public class Rock : MovingObject {
 		// negative number in a - b you will get incorrect information
 		//mathf.abs(-10) is 10
 		//local pos - where we currently are
-
+		
 		//.y because we only want the y pos
 		while(Mathf.Abs((target-transform.localPosition).y) > 0.20f){
-		//	print ((target-transform.localPosition).y);
-
+			//	print ((target-transform.localPosition).y);
+			
 			Vector3 direction = target.y == topPos.y ? Vector3.up : Vector3.down;
 			// if target.y is equal to top position. y then do vector 3 up if not then do vector 3 down
 			// ternary operations are better than if statements imo
 			transform.localPosition += direction * Time.deltaTime * speed;
-
-			// add this direction to the position
-
-
 			
-
+			// add this direction to the position
+			
+			
+			
+			
 			yield return null;
-
-
+			
+			
 		}
-
-		yield return new WaitForSeconds (0.5f);
-
+		
+		yield return new WaitForSeconds (0.25f);
+		
 		Vector3 newTarget = target.y == topPos.y ? bottPos : topPos;
-
+		
 		StartCoroutine (Move (newTarget));
 	}
 }
